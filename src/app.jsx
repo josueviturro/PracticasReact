ac3b87d714e9bfcb98849c9959af90fda4a070a3
josueviturro <josueviturro@gmail.com>
2025-10-21 01:07:05 -0300

    Se agregaron las rutas de react-rutes-dom

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Hello from './modules/Hello.jsx'
 import Contador from './modules/Contador.jsx'
 import Formulario from './modules/Formulario.jsx';
@@ -13,15 +14,30 @@ export default function App() {
     return (
     <main>
         <h1>React Playground</h1>
-        <Hello nombre="Josue" />
-        <Contador />
-        <Formulario />
-        <Tareas />
-        <TareasGuardadas />
-        <Usuarios />
-        <UsuariosCRUD />
-        <UsuariosDelete />
-        <UsuariosPatch />
+        <BrowserRouter>
+            <nav style={{marginBottom: "20px", gap: "10px", display: "flex", flexWrap: "wrap"}}>
+                <Link to="/" ><button>Módulo 01</button></Link>
+                <Link to="/contador" ><button>Módulo 02</button></Link>
+                <Link to="/formulario" ><button>Módulo 03</button></Link>
+                <Link to="/tareas" ><button>Módulo 04</button></Link>
+                <Link to="/tareasguardadas" ><button>Módulo 05</button></Link>
+                <Link to="/usuarios" ><button>Módulo 06</button></Link>
+                <Link to="/usuarioscrud" ><button>Módulo 07</button></Link>
+                <Link to="/usuariosdelete" ><button>Módulo 08</button></Link>
+                <Link to="/usuariospatch" ><button>Módulo 09</button></Link>
+            </nav>
+            <Routes>
+                <Route path='/' element={<Hello nombre="Josue" />}/>
+                <Route path='/contador' element={<Contador />}/>
+                <Route path='/formulario' element={<Formulario />}/>
+                <Route path='/tareas' element={<Tareas />}/>
+                <Route path='/tareasguardadas' element={<TareasGuardadas />}/>
+                <Route path='/usuarios' element={<Usuarios />}/>
+                <Route path='/usuarioscrud' element={<UsuariosCRUD />}/>
+                <Route path='/usuariosdelete' element={<UsuariosDelete />}/>
+                <Route path='/usuariospatch' element={<UsuariosPatch />}/>
+            </Routes>
+        </BrowserRouter>
     </main>
     );
 }
